33a03ea96d1cbcfda09cb7c429a893681d863b3c
Fide Omar Islas Duarte <fidev.id@outlook.com>
2022-02-01 17:20:25 -0600

    Integrate Facebook Login flow

--- android/app/src/main/java/com/fidev/sociallogin/MainActivity.java
+++ android/app/src/main/java/com/fidev/sociallogin/MainActivity.java
@@ -1,5 +1,15 @@
 package com.fidev.sociallogin;
 
+import android.os.Bundle;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    registerPlugin(com.getcapacitor.community.facebooklogin.FacebookLogin.class);
+  }
+
+}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FacebookLogin } from '@capacitor-community/facebook-login';
+import { HttpClient } from '@angular/common/http';
+
+const FACEBOOK_PERMISSIONS = ['email', 'user_birthday', 'user_photos', 'user_gender'];
 
 @Component({
   selector: 'app-home',
@@ -6,7 +10,52 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  user = null;
+  token = null;
 
-  constructor() {}
+  constructor(private http: HttpClient) { }
+
+  async login() {
+    console.log("Loging...");
+    const result = await FacebookLogin.login({ permissions: FACEBOOK_PERMISSIONS });
+    console.log(`Facebook Login response`);
+    console.log({ result });
+
+    if (result.accessToken && result.accessToken.userId) {
+      this.token = result.accessToken;
+    } else if (result.accessToken && !result.accessToken.userId) {
+      this.getCurrentToken();
+    } else console.error("Login error");
+  }
+
+  async getCurrentToken() {
+    const result = await FacebookLogin.getCurrentAccessToken();
+    console.log("Current Token response");
+    console.log({ result });
+
+    if (result.accessToken) {
+      this.token = result.accessToken;
+      this.loadUserData();
+    } else {
+      console.error("Current token error");
+    }
+  }
+
+  async loadUserData() {
+    const url = `https://graph.facebook.com/${this.token.userId}?fields=id,name,picture.width(720),birthday,email&access_token=${this.token.token}`;
+    this.http.get(url).subscribe(user => {
+      console.log({ user });
+      this.user = user;
+    });
+  }
+
+  logout() {
+    console.log("Logout...");
+    FacebookLogin.logout().then(() => {
+      console.log("Success logout");
+      this.token = null;
+      this.user = null;
+    }).catch(() => console.log("Fail Logout"));
+  }
 
 }
